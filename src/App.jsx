b4574c1054497d875ee227ff0ed84330ebbfc1d1
citronoiseau <avroraka2004@gmail.com>
2025-05-02 21:57:55 +0000

    made cart globally acessible

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css';
 import { Outlet } from 'react-router-dom'; 
 import StoreAPI from "./utils/StoreAPI";
 import NavBar from './components/navbar/NavBar';
-
+import { CartProvider } from './components/CartData';
 function App() {
   const shopItems = StoreAPI();
   
   return (
     <>
+    <CartProvider> 
       <NavBar />
       <Outlet context={{ shopItems }} /> 
+      </CartProvider>
     </>
   )
 }
