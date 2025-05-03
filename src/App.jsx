c68dc2f8b4532ed477141ced7fbf4fa060053e7a
citronoiseau <avroraka2004@gmail.com>
2025-05-03 17:00:09 +0000

    moved card provider to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import { Outlet } from 'react-router-dom';
 import StoreAPI from "./utils/StoreAPI";
 import NavBar from './components/navbar/NavBar';
 import { CartProvider } from './utils/CartData';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const items = StoreAPI();
   const clothingItems = items.filter(item => item.category !== "electronics");
 
   return (
-    <>
-    <CartProvider> 
-      <NavBar />
-      <Outlet context={{ clothingItems }} /> 
+    <div className="appContainer">
+      <CartProvider> 
+        <NavBar />
+        <main className="mainContent">
+          <Outlet context={{ clothingItems }} />
+        </main>
+        <Footer />
       </CartProvider>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
-import { CartProvider } from './components/CartData';
+
 
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CartProvider>
     <RouterProvider router={router} />
-    </CartProvider>
   </StrictMode>,
 );
