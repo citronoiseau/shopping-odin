213eb059334e40a747280a340a24f1f4f6a38d47
citronoiseau <avroraka2004@gmail.com>
2025-05-03 16:16:37 +0000

    finished item card design

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,15 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import styles from "./ItemCard.module.css";
 
 const itemCard = (item) => {
-    return (
-        <div>
-            <div className={styles.imageContainer}>
-                <img className={styles.img} src={item.item.image} />
-            </div>
-            <div> {item.item.title} </div>
-            <div> {item.item.price} </div>
+    const navigate = useNavigate();
 
-        </div>
+    const handleClick = () => {
+        navigate(`/shop/clothes/${item.item.id}`);
+      };
+
+    return (
+        <button className={styles.itemContainer} onClick={handleClick}>
+            <div className={styles.imageContainer}>
+              <img className={styles.img} src={item.item.image} />
+            </div>
+            <div className={styles.itemDetails}>
+              <div className={styles.title}>{item.item.title}</div>
+              <div className={styles.price}>${item.item.price}</div>
+            </div>
+      </button>
 
     )
 }
