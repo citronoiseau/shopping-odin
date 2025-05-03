3721687d3a656e19db50667f0cb14ac06004975b
citronoiseau <avroraka2004@gmail.com>
2025-05-03 21:02:22 +0000

    fixed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div> Inspired by Levis website</div>
+      <div> Inspired by Levi's website</div>
       <div> Made with love by  <a href="https://github.com/citronoiseau" target="_blank" > citronoiseau </a></div>
     </footer>
   );
