72a40609d6095c78212e9e0452c9fb7aad9db5bd
citronoiseau <avroraka2004@gmail.com>
2025-05-04 16:31:29 +0000

    test if app.jsx renders everything correctly

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,35 @@
+import { describe, it, vi, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
+vi.mock('./utils/StoreAPI', () => ({
+  default: () => [
+    { id: 1, category: 'clothing', name: 'Shirt' },
+    { id: 2, category: 'clothing', name: 'Pants' },
+    { id: 3, category: 'electronics', name: 'Phone' }
+  ]
+}));
 
-    screen.debug();
 
-    // check if App components renders headline
+function MockComponent() {
+  return <div data-testid="mock-page">Mock Page</div>;
+}
+
+describe('App Component', () => {
+  it('renders NavBar, Mock Outlet, and Footer', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<MockComponent />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByTestId('mock-page')).toBeInTheDocument();
+    expect(screen.getByRole('navigation')).toBeInTheDocument(); 
+    expect(screen.getByRole('contentinfo')).toBeInTheDocument();
   });
 });
